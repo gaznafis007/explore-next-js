77b2651d691169e38d12f2c4123cb6b0e51e86bf
gazinafis007 <gazinafisrafi.gnr@gmail.com>
2024-01-17 08:20:55 +0600

    active links

--- app/(auth)/layout.js
+++ app/(auth)/layout.js
@@ -1,17 +1,31 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 export default function AuthLayout({ children }) {
+  const pathName = usePathname();
   return (
     <div>
       <nav className="mt-4 text-slate-800 font-semibold capitalize flex flex-col md:flex-row ">
-        <Link className="mx-2" href={"/signIn"}>
+        <Link
+          className={`mx-2 ${pathName === "/signIn" ? "text-sky-400" : ""}`}
+          href={"/signIn"}
+        >
           sign in
         </Link>
-        <Link className="mx-2" href={"/register"}>
+        <Link
+          className={`mx-2 ${pathName === "/register" ? "text-sky-400" : ""}`}
+          href={"/register"}
+        >
           register
         </Link>
-        <Link className="mx-2" href={"/forgot-password"}>
+        <Link
+          className={`mx-2 ${
+            pathName === "/forgot-password" ? "text-sky-400" : ""
+          }`}
+          href={"/forgot-password"}
+        >
           forgot password
         </Link>
       </nav>
